e92c0944f7d902c138e115a2f5d0ba383c88776c
thaidev13639 <pnt0964413557@gmail.com>
2024-04-23 16:59:14 +0700

    update animation client navbar

--- src/pages/Project/ProjectUserBoard.tsx
+++ src/pages/Project/ProjectUserBoard.tsx
@@ -3,26 +3,32 @@ import { useRef, useState } from "react";
 import ClientAppSidebar from "../../components/Client/ClientAppSidebar";
 
 export default function ProjectUserBoard() {
-    const sidebarRef = useRef(null);
-    const [isOpen, setIsOpen] = useState(true);
-    const toggleSidebar = () => {
-        setIsOpen(!isOpen);
-    };
-    return (
-        <div >
-            <div className={`client-layout-sidebar h-full z-3 ${isOpen ? '' : 'hidden'}`}>
-                <div ref={sidebarRef}>
-                    <ClientAppSidebar />
-                </div>
-            </div>
-            <Button
-                onClick={toggleSidebar}
-                icon={isOpen ? 'pi pi-times' : 'pi pi-bars'}
-                className="p-button-rounded p-button-text p-mr-2 p-mt-2"
-                aria-label="Toggle sidebar"
-            />
+  const sidebarRef = useRef(null);
+  const [isOpen, setIsOpen] = useState(true);
+  const toggleSidebar = () => {
+    setIsOpen(!isOpen);
+  };
+  return (
+    <div className="relative flex justify-content-start align-items-start">
+      <div
+        className={`client-layout-sidebar h-screen z-3 ${
+          isOpen ? "" : "hidden__navbarClient"
+        }`}>
+        <div className="client-layout-sidebar-content" ref={sidebarRef}>
+          <ClientAppSidebar />
         </div>
+      </div>
 
-
-    )
+      <div className="relative right-content">
+        <div className="absolute button__navClient">
+          <Button
+            onClick={toggleSidebar}
+            icon={`z-3 pi ${isOpen ? "pi-angle-left" : "pi-angle-right"}`}
+            className="p-button-rounded p-button-text p-mr-2 p-mt-2"
+            aria-label="Toggle sidebar"></Button>
+        </div>
+        Content
+      </div>
+    </div>
+  );
 }
